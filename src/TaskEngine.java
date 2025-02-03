4f46827045ff2ae6caefd2000f152acc76e5f5d1
fkbko <a_0206@outlook.com>
2025-02-03 16:19:43 +0300

    -

--- src/TaskEngine.java
+++ src/TaskEngine.java
@@ -4,11 +4,11 @@ public class TaskEngine implements Serializable {
     private String key;
     private String title;
     private String description;
-    private static int idCounter = 0;
+    private static int keyCounter = 0;
 
     public TaskEngine(String title, String description) {
-        int i = idCounter++;
-        this.key = String.valueOf(i);
+        int k = keyCounter++;
+        this.key = String.valueOf(k);
         this.title = title;
         this.description = description;
 
